39bc729ab1c0c65f46465189b375dcad6a965fb3
Alexey Kuzmin <kuzall1991@gmail.com>
2016-05-03 22:07:53 +0300

    upd: Same changing

--- stepic.org/Web_Technologies/1.6-4a.py
+++ stepic.org/Web_Technologies/1.6-4a.py
@@ -1,7 +1,7 @@
 import socket
 inp = '0'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("127.0.0.1", 1234))
+s.connect(("127.0.0.1", 2222))
 while inp != 'exit':
     s.send(inp.encode())
     print(s.recv(1024).decode())
